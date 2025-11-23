e19a340491bba2a9c4f17317c17eeeeda8e827cd
abhinandhsunil1 <abhinandh20nov@gmail.com>
2025-11-22 19:57:25 -0500

    Update server.js

--- Group Assignment/server.js
+++ Group Assignment/server.js
@@ -3,6 +3,14 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 // Serve static frontend files from /public
 app.use(express.static('public'));
 
